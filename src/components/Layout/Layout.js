c9bd60fb3f12221c5e5ee6c60d7930ffd5259aa1
mjr68 <mjr68@duke.edu>
2020-04-15 19:35:52 -0400

    basic implementation with price, modals, and navigation for desktop and mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,37 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-	<Fragment>
-		<div>
-			Toolbar, SideDrawer, Backdrop
-		</div>
-		<main className={classes.Content}>
-			{props.children}
-		</main>
-	</Fragment>
-);
+class Layout extends Component {
 
-export default layout;
+	state = {
+		showSideDrawer: false
+	}
+
+	sideDrawerCloseHandler = () => {
+		this.setState({showSideDrawer: false});
+	}
+
+	sideDrawerToggleHandler = () => {
+		this.setState( (prevState) => {
+			return {showSideDrawer: !prevState.showSideDrawer};
+		})
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<Toolbar menuClick={this.sideDrawerToggleHandler}/>
+				<SideDrawer 
+					open={this.state.showSideDrawer} 
+					closed={this.sideDrawerCloseHandler} />
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</Fragment>
+		)
+	}
+}
+
+export default Layout;
